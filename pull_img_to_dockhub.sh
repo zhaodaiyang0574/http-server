8e3fa365e9c2ff5593976989ca110cf1f6e86b36
zhaodaiyang0574 <zhaolu0574@sina.com>
2022-07-31 18:10:23 +0800

    Update pull_img_to_dockhub.sh

--- pull_img_to_dockhub.sh
+++ pull_img_to_dockhub.sh
@@ -1,7 +1,14 @@
 
-#use command:
-#sh pull_img_to_dockhub.sh httpserver v1.0
+###Build docker
+#docker build -t httpserver:v1.0 -f dockerfile  .
 
+
+###start project
+#docker run -d httpserver:v1.0 -p 8080:8080
+
+
+###Pull image to dockerhub
+#sh pull_img_to_dockhub.sh httpserver v1.0
 REPOSITORY = "registry.cn-beijing.aliyuncs.com"
 PROJECT = "$1"
 IMAGE_NAME = "$1:$2"
