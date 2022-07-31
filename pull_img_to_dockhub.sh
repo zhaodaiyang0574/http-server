cf0de6df9595505a59a81a8fd802d625dcc6f630
zhaodaiyang0574 <zhaolu0574@sina.com>
2022-07-31 18:03:01 +0800

    Create pull_img_to_dockhub.sh

---
+++ pull_img_to_dockhub.sh
@@ -0,0 +1,11 @@
+
+#use command:
+#sh pull_img_to_dockhub.sh httpserver v1.0
+
+REPOSITORY = "registry.cn-beijing.aliyuncs.com"
+PROJECT = "$1"
+IMAGE_NAME = "$1:$2"
+
+sudo docker login --username=fernandoander -p xxx $REPOSITORY
+sudo docker tag ${IMAGE_NAME} $REPOSITORY/$PROJECT/${IMAGE_NAME}
+sudo docker push  $REPOSITORY/$PROJECT/${IMAGE_NAME}
