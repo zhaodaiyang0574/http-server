4b20825d5cadb0eab2b4835b70e7e7317affda63
zhaodaiyang0574 <zhaolu0574@sina.com>
2022-07-31 18:07:04 +0800

    Update pull_img_to_dockhub.sh

--- pull_img_to_dockhub.sh
+++ pull_img_to_dockhub.sh
@@ -6,6 +6,6 @@ REPOSITORY = "registry.cn-beijing.aliyuncs.com"
 PROJECT = "$1"
 IMAGE_NAME = "$1:$2"
 
-sudo docker login --username=fernandoander -p [password] $REPOSITORY
-sudo docker tag ${IMAGE_NAME} $REPOSITORY/$PROJECT/${IMAGE_NAME}
-sudo docker push  $REPOSITORY/$PROJECT/${IMAGE_NAME}
+sudo docker login --username=fernandoander -p [password] ${REPOSITORY}
+sudo docker tag ${IMAGE_NAME} ${REPOSITORY}/${PROJECT}/${IMAGE_NAME}
+sudo docker push  ${REPOSITORY}/${PROJECT}/${IMAGE_NAME}
